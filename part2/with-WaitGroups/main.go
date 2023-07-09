f41daf84e7734d6c7fe72f4b9e4974db30a75e32
phuongdoan13 <mphuongdoant@gmail.com>
2023-07-09 22:11:00 +1000

    Fixed

--- part2/with-WaitGroups/main.go
+++ part2/with-WaitGroups/main.go
@@ -4,11 +4,13 @@ import (
   "fmt"
   "runtime"
   "time"
+	"sync"
 )
 
 func main() {
   fmt.Println("number of cores", runtime.NumCPU())
   var wg sync.WaitGroup
+	wg.Add(10)
   for i:=0; i<10; i++ {
     go work(&wg, i+1)
   }
